e131b33a1f53f08e6e7e58b09d2df37cc6156759
winterrdog <winterrdog@protonmail.ch>
2024-04-09 12:30:11 +0300

    feat: delete a comment

--- src/controllers/comment.ts
+++ src/controllers/comment.ts
@@ -110,11 +110,51 @@ const commentController = {
     // todo
     res.send("Update a comment");
   },
-
-  // DELETE /:postId/comments/:id
   deleteComment: async function (req: Request, res: Response) {
-    // todo
-    res.send("Delete a comment");
+    try {
+      const { postId, id: commentId } = req.params;
+      if (!isValidObjectId(postId)) {
+        return res.status(400).json({
+          message: `Post id, ${postId}, is invalid or malformed.`,
+        });
+      }
+      if (!isValidObjectId(commentId)) {
+        return res.status(400).json({
+          message: `Comment id, ${commentId}, is invalid or malformed.`,
+        });
+      }
+
+      const storedComment = await CommentModel.findOne({
+        _id: commentId,
+        post: postId,
+      });
+      if (!storedComment) {
+        return res.status(404).json({
+          message: `comment with id, ${commentId}, and post id, ${postId}, was not found.`,
+        });
+      }
+
+      // check author
+      const { data } = (req.user! as any).data as JwtPayload;
+      const { sub } = data;
+      if (sub !== storedComment.user.toHexString()) {
+        return res.status(403).json({
+          message:
+            "you are not the author of this post so you will not delete it",
+        });
+      }
+
+      await storedComment.deleteOne();
+      return res.status(204).json({});
+    } catch (e) {
+      console.error(`Error deleting a comment: ${e}`);
+      return res.status(500).json({
+        message:
+          process.env.NODE_ENV === "production"
+            ? "Internal server error occurred. Please try again later."
+            : (e as Error).message,
+      });
+    }
   },
 };
 
