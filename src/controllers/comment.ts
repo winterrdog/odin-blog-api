5a36ddeb790cb29d7c08299ffffc07a2b594be8d
winterrdog <winterrdog@protonmail.ch>
2024-04-09 12:05:59 +0300

    feat: get all post comments

--- src/controllers/comment.ts
+++ src/controllers/comment.ts
@@ -37,11 +37,36 @@ const commentController = {
       });
     }
   },
-
-  // GET /:postId/comments
   getComments: async function (req: Request, res: Response) {
-    // todo
-    res.send("Get all comments");
+    try {
+      const { postId } = req.params;
+      if (!isValidObjectId(postId)) {
+        return res.status(400).json({
+          message: `Post id, ${postId}, is invalid or malformed.`,
+        });
+      }
+
+      const storedComments = await CommentModel.find({
+        post: postId,
+      });
+      if (storedComments.length === 0) {
+        return res.status(200).json({
+          message: `No comments available for post with id, ${postId}.`,
+        });
+      }
+      return res.status(200).json({
+        message: "post comments fetched successfully",
+        comments: storedComments,
+      });
+    } catch (e) {
+      console.error(`Error fetching comments: ${e}`);
+      return res.status(500).json({
+        message:
+          process.env.NODE_ENV === "production"
+            ? "Internal server error occurred. Please try again later."
+            : (e as Error).message,
+      });
+    }
   },
 
   // POST /:postId/comments
