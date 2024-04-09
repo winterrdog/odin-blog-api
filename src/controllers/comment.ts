25919e796602727f7a68354f42dad75411715d1e
winterrdog <winterrdog@protonmail.ch>
2024-04-09 11:53:29 +0300

    added `get single comment` controller

--- src/controllers/comment.ts
+++ src/controllers/comment.ts
@@ -1,32 +1,63 @@
 import { Request, Response } from "express";
+import { isValidObjectId } from "mongoose";
+import { CommentModel } from "../models/comment";
 
 const commentController = {
-  // GET /:postId/comments/:id
-  getCommentById: function (req: Request, res: Response) {
-    // todo
-    res.send("Get a comment by id");
+  getCommentById: async function (req: Request, res: Response) {
+    try {
+      const { postId, id: commentId } = req.params;
+      if (!isValidObjectId(postId)) {
+        return res.status(400).json({
+          message: `Post id, ${postId}, is invalid or malformed.`,
+        });
+      }
+      if (!isValidObjectId(commentId)) {
+        return res.status(400).json({
+          message: `Comment id, ${commentId}, is invalid or malformed.`,
+        });
+      }
+
+      const storedComment = await CommentModel.findOne({
+        _id: commentId,
+        post: postId,
+      });
+      if (!storedComment) {
+        return res.status(404).json({
+          message: `comment with id, ${commentId}, and post id, ${postId}, was not found.`,
+        });
+      }
+      return storedComment;
+    } catch (e) {
+      console.error(`Error fetching comment by id: ${e}`);
+      return res.status(500).json({
+        message:
+          process.env.NODE_ENV === "production"
+            ? "Internal server error occurred. Please try again later."
+            : (e as Error).message,
+      });
+    }
   },
 
   // GET /:postId/comments
-  getComments: function (req: Request, res: Response) {
+  getComments: async function (req: Request, res: Response) {
     // todo
     res.send("Get all comments");
   },
 
   // POST /:postId/comments
-  createComment: function (req: Request, res: Response) {
+  createComment: async function (req: Request, res: Response) {
     // todo
     res.send("Create a new comment");
   },
 
   // PATCH /:postId/comments/:id
-  updateComment: function (req: Request, res: Response) {
+  updateComment: async function (req: Request, res: Response) {
     // todo
     res.send("Update a comment");
   },
 
   // DELETE /:postId/comments/:id
-  deleteComment: function (req: Request, res: Response) {
+  deleteComment: async function (req: Request, res: Response) {
     // todo
     res.send("Delete a comment");
   },
