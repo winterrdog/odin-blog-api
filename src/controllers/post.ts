6604e1c5b55d5044541b2950144a6576bea77c6d
winterrdog <winterrdog@protonmail.ch>
2024-04-11 07:16:18 +0300

    feat: added validation and sanitization to posts

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -3,42 +3,48 @@ import _ from "lodash";
 import { Types, isValidObjectId } from "mongoose";
 import { PostModel } from "../models/post";
 import { JwtPayload } from "../middleware/interfaces";
-
-// todo: introduce data validation and sanitization
+import { postIdSanitizer } from "../validators/comments";
+import Utility from "../utilities";
+import { postReqBodyValidators } from "../validators/post";
+import { matchedData } from "express-validator";
 
 function isAuthorSame(tgtAuthor: Types.ObjectId, reqAuthor: JwtPayload) {
-  return tgtAuthor.toString() === reqAuthor.data.sub;
+  return tgtAuthor.toHexString() === reqAuthor.data.sub;
 }
 
 const postController = {
-  getPostById: async function (req: Request, res: Response) {
-    try {
-      const { id } = req.params;
-      if (isValidObjectId(id) === false) {
-        return res.status(400).json({ message: "Invalid post id" });
-      }
+  getPostById: [
+    postIdSanitizer,
+    Utility.validateRequest,
+    async function (req: Request, res: Response) {
+      try {
+        const { id } = req.params;
+        if (isValidObjectId(id) === false) {
+          return res.status(400).json({ message: "Invalid post id" });
+        }
 
-      // check if post exists
-      const post = await PostModel.findById(id);
-      if (!post) {
-        return res
-          .status(404)
-          .json({ message: `Post with id ${id} not found` });
+        // check if post exists
+        const post = await PostModel.findById(id);
+        if (!post) {
+          return res
+            .status(404)
+            .json({ message: `Post with id ${id} not found` });
+        }
+        return res.status(200).json({
+          message: "Post retrieved successfully",
+          post,
+        });
+      } catch (e) {
+        console.error(`Error occurred during fetching post by id: ${e}`);
+        return res.status(500).json({
+          message:
+            process.env.NODE_ENV === "production"
+              ? "Internal server error occurred. Please try again later."
+              : (e as Error).message,
+        });
       }
-      return res.status(200).json({
-        message: "Post retrieved successfully",
-        post,
-      });
-    } catch (e) {
-      console.error(`Error occurred during fetching post by id: ${e}`);
-      return res.status(500).json({
-        message:
-          process.env.NODE_ENV === "production"
-            ? "Internal server error occurred. Please try again later."
-            : (e as Error).message,
-      });
-    }
-  },
+    },
+  ],
   getPosts: async function (_req: Request, res: Response) {
     try {
       const posts = await PostModel.find({});
@@ -61,112 +67,127 @@ const postController = {
       });
     }
   },
-  createPost: async function (req: Request, res: Response) {
-    try {
-      // grab user id from jwt
-      const jwtData = (req.user! as any).data as JwtPayload;
-      const { sub } = jwtData.data;
+  createPost: [
+    ...postReqBodyValidators,
+    Utility.validateRequest,
+    async function (req: Request, res: Response) {
+      try {
+        // grab user id from jwt
+        const jwtData = (req.user! as any).data as JwtPayload;
+        const { sub } = jwtData.data;
 
-      // create post
-      const post = await PostModel.create({
-        author: sub,
-        ...req.body,
-      });
+        // create post
+        const post = await PostModel.create({
+          author: sub,
+          ...req.body,
+        });
 
-      return res.status(201).json({
-        message: "Post created successfully",
-        post,
-      });
-    } catch (e) {
-      console.error(`Error occurred during creating post: ${e}`);
-      return res.status(500).json({
-        message:
-          process.env.NODE_ENV === "production"
-            ? "Internal server error occurred. Please try again later."
-            : (e as Error).message,
-      });
-    }
-  },
-  updatePost: async function (req: Request, res: Response) {
-    try {
-      // grab post id from request params
-      const { id } = req.params;
-      if (isValidObjectId(id) === false) {
-        return res.status(400).json({ message: "Invalid post id" });
-      }
-
-      let post = await PostModel.findById(id);
-      if (!post) {
-        return res
-          .status(404)
-          .json({ message: `Post with id ${id} not found` });
-      }
-
-      // check if user is the author of the post
-      const jwtData = (req.user! as any).data as JwtPayload;
-      if (!isAuthorSame(post.author, jwtData)) {
-        return res.status(403).json({
-          message: "You are not authorized to update this post",
+        return res.status(201).json({
+          message: "Post created successfully",
+          post,
+        });
+      } catch (e) {
+        console.error(`Error occurred during creating post: ${e}`);
+        return res.status(500).json({
+          message:
+            process.env.NODE_ENV === "production"
+              ? "Internal server error occurred. Please try again later."
+              : (e as Error).message,
         });
       }
+    },
+  ],
+  updatePost: [
+    postIdSanitizer,
+    ...postReqBodyValidators,
+    Utility.validateRequest,
+    async function (req: Request, res: Response) {
+      try {
+        // grab post id from sanitized request params
+        const sanitizedData = matchedData(req);
+        const { id } = sanitizedData;
+        if (isValidObjectId(id) === false) {
+          return res.status(400).json({ message: "Invalid post id" });
+        }
 
-      // merge data to update
-      post = _.merge(post, req.body);
-      await post!.save();
+        let post = await PostModel.findById(id);
+        if (!post) {
+          return res
+            .status(404)
+            .json({ message: `Post with id ${id} not found` });
+        }
 
-      return res.status(200).json({
-        message: "Post updated",
-        post,
-      });
-    } catch (e) {
-      console.error(`Error occurred during updating post: ${e}`);
-      return res.status(500).json({
-        message:
-          process.env.NODE_ENV === "production"
-            ? "Internal server error occurred. Please try again later."
-            : (e as Error).message,
-      });
-    }
-  },
-  deletePost: async function (req: Request, res: Response) {
-    try {
-      const { id } = req.params;
-      if (isValidObjectId(id) === false) {
-        return res.status(400).json({ message: "Invalid post id" });
-      }
+        // check if user is the author of the post
+        const jwtData = (req.user! as any).data as JwtPayload;
+        if (!isAuthorSame(post.author, jwtData)) {
+          return res.status(403).json({
+            message: "You are not authorized to update this post",
+          });
+        }
 
-      // check if post exists
-      const post = await PostModel.findById(id);
-      if (!post) {
-        return res
-          .status(404)
-          .json({ message: `Post with id ${id} not found` });
-      }
+        // merge data to update
+        post = _.merge(post, req.body);
+        await post!.save();
 
-      // check if user is the author of the post
-      const jwtData = (req.user! as any).data as JwtPayload;
-      if (!isAuthorSame(post.author, jwtData)) {
-        return res.status(403).json({
-          message: "You are not authorized to update this post",
+        return res.status(200).json({
+          message: "Post updated",
+          post,
+        });
+      } catch (e) {
+        console.error(`Error occurred during updating post: ${e}`);
+        return res.status(500).json({
+          message:
+            process.env.NODE_ENV === "production"
+              ? "Internal server error occurred. Please try again later."
+              : (e as Error).message,
         });
       }
+    },
+  ],
+  deletePost: [
+    postIdSanitizer,
+    Utility.validateRequest,
+    async function (req: Request, res: Response) {
+      try {
+        const sanitizedData = matchedData(req);
+        const { id } = sanitizedData;
+        if (isValidObjectId(id) === false) {
+          return res.status(400).json({ message: "Invalid post id" });
+        }
 
-      // delete post
-      await post.deleteOne();
+        // check if post exists
+        const post = await PostModel.findById(id);
+        if (!post) {
+          return res
+            .status(404)
+            .json({ message: `Post with id ${id} not found` });
+        }
 
-      return res.status(204).json({
-        message: "Post deleted successfully",
-      });
-    } catch (e) {
-      console.error(`Error occurred during deleting post: ${e}`);
-      return res.status(500).json({
-        message:
-          process.env.NODE_ENV === "production"
-            ? "Internal server error occurred. Please try again later."
-            : (e as Error).message,
-      });
-    }
-  },
+        // check if user is the author of the post
+        const jwtData = (req.user! as any).data as JwtPayload;
+        if (!isAuthorSame(post.author, jwtData)) {
+          return res.status(403).json({
+            message: "You are not authorized to update this post",
+          });
+        }
+
+        // delete post
+        await post.deleteOne();
+
+        return res.status(204).json({
+          message: "Post deleted successfully",
+        });
+      } catch (e) {
+        console.error(`Error occurred during deleting post: ${e}`);
+        return res.status(500).json({
+          message:
+            process.env.NODE_ENV === "production"
+              ? "Internal server error occurred. Please try again later."
+              : (e as Error).message,
+        });
+      }
+    },
+  ],
 };
 
 export default postController;
---
+++ src/validators/post.ts
@@ -0,0 +1,24 @@
+import { body, param } from "express-validator";
+
+export const postIdSanitizer = param("id").trim().escape();
+
+// validate and sanitize body
+export const titleValidator = body("title")
+  .trim()
+  .isLength({
+    min: 4,
+    max: 56,
+  })
+  .withMessage("title should have 4 to 56 characters.")
+  .escape();
+export const postBodyValidator = body("body")
+  .trim()
+  .notEmpty()
+  .withMessage("body has to be provided.")
+  .escape();
+export const hiddenValidator = body("hidden").trim().optional().escape();
+export const postReqBodyValidators = [
+  titleValidator,
+  postBodyValidator,
+  hiddenValidator,
+];
