e9fa9f81733a020b491c305a0688321b9036ae30
winterrdog <winterrdog@protonmail.ch>
2024-04-11 20:37:16 +0300

    feat: added connection to database

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -2,8 +2,10 @@
  * Module dependencies.
  */
 
+import mongoose from "mongoose";
 import app from "../app";
 import http from "node:http";
+require("dotenv").config(); // env variables
 
 const debug = require("debug")("src:server");
 
@@ -11,7 +13,7 @@ const debug = require("debug")("src:server");
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || "3000");
+const port = normalizePort(process.env.PORT! || "3000");
 app.set("port", port);
 
 /**
@@ -19,14 +21,26 @@ app.set("port", port);
  */
 
 const server = http.createServer(app);
+let conn: mongoose.Connection | null = null;
 
 /**
  * Listen on provided port, on all network interfaces.
  */
+(async function () {
+  try {
+    server.listen(port);
 
-server.listen(port);
-server.on("error", onError);
-server.on("listening", onListening);
+    conn = await connectToDb();
+    if (!conn) throw new Error("database didn't connect successfully");
+
+    server.on("error", onError);
+    server.on("listening", onListening);
+  } catch (e) {
+    debug("server crashed: ", e);
+    if (conn) await closeDb(conn);
+    process.exit(1);
+  }
+})();
 
 /**
  * Normalize a port into a number, string, or false.
@@ -47,20 +61,23 @@ function normalizePort(val: string) {
 /**
  * Event listener for HTTP server "error" event.
  */
-
 function onError(error: { syscall: string; code: string }) {
   if (error.syscall !== "listen") throw error;
+  if (!conn) throw new Error("database didn't connect successfully");
 
+  closeDb(conn).catch((e) => {
+    debug("mongodb ran into an error during close: ", e);
+  });
   const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
+      debug(bind + " requires elevated privileges");
+      return;
     case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
+      debug(bind + " is already in use");
+      return;
     default:
       throw error;
   }
@@ -76,4 +93,34 @@ function onListening() {
 
   const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
   debug("Listening on " + bind);
+}
+
+/**
+ * Start the database connection
+ */
+async function connectToDb() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI!);
+    debug("database server was connected to successfully!");
+    return mongoose.connection;
+  } catch (e) {
+    debug("Error occurred when connceting to database: ", e);
+    return null;
+  }
+}
+
+/**
+ * Closes the database connection.
+ * @param conn mongoose.Connection
+ * @returns void
+ */
+async function closeDb(conn: mongoose.Connection) {
+  try {
+    conn.on("disconnecting", () => {
+      debug("application disconnecting from mongodb...");
+    });
+    await conn.close();
+  } catch (e) {
+    debug("error occurred after connecting to db: ", e);
+  }
 }
