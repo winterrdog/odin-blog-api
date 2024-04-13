0a94e647d79b7d0426f5971f8eda688a071f8cbb
winterrdog <winterrdog@protonmail.ch>
2024-04-13 13:49:47 +0300

    feat: added log transportation to file

--- src/logging/logger.ts
+++ src/logging/logger.ts
@@ -1,22 +1,51 @@
 import pino, { LoggerOptions } from "pino";
+import Utility from "../utilities";
+require("dotenv").config(); // env vars
+
+// transport logs to external file and console
+const logFile = `./${process.env.LOGS_FILE ?? "app.log"}`;
+createLogFile(logFile);
+const logTransports = pino.transport({
+  targets: [
+    {
+      target: "pino-pretty",
+      options: {
+        levelFirst: true,
+        colorize: true,
+        translateTime: "dd/mm/yyyy HH:MM:ss.L TT Z",
+      },
+    },
+    {
+      target: "pino/file",
+      level: "trace",
+      options: {
+        destination: logFile,
+      },
+    },
+  ],
+});
+
 const defaultLoggingOptions: LoggerOptions = {
   level: process.env.LOG_LEVEL ?? "info",
-  transport: {
-    target: "pino-pretty",
-    // pino-pretty options
-    options: {
-      levelFirst: true,
-      colorize: true,
-      translateTime: "dd/mm/yyyy HH:MM:ss.L TT Z",
-    },
-  },
+  timestamp: pino.stdTimeFunctions.isoTime,
 };
 function getModuleName(fname: string): string {
   return fname.split("/").at(-1)!.split(".")[0] + ": ";
 }
 export function startLogger(fname: string) {
-  return pino({
-    ...defaultLoggingOptions,
-    msgPrefix: "blog-api:" + getModuleName(fname),
-  });
+  return pino(
+    {
+      ...defaultLoggingOptions,
+      msgPrefix: "blog-api:" + getModuleName(fname),
+    },
+    logTransports
+  );
+}
+async function createLogFile(f: string) {
+  try {
+    await Utility.createFile(f);
+  } catch (err) {
+    console.error("failed to create an external log file: ", err);
+    process.exit(1);
+  }
 }
--- src/utilities.ts
+++ src/utilities.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import * as jwt from "jsonwebtoken";
 import { validationResult } from "express-validator";
 import { JwtPayload } from "./middleware/interfaces";
+import { PathLike } from "fs";
+import * as fs from "fs";
 
 export default class Utility {
   static generateJwtPayload(payload: JwtPayload): Promise<string> {
@@ -24,5 +26,24 @@ export default class Utility {
     return errors.isEmpty()
       ? next()
       : res.status(400).json({ errors: errors.array() });
+  }
+  static createFile(fname: PathLike): Promise<void> {
+    // create a file if it doesn't exist
+    // if it exists, do nothing
+    return new Promise((resolve, reject) => {
+      // "wx" flag creates a file if it doesn't exist
+      fs.open(fname, "wx", (err, fd) => {
+        if (err) {
+          if (err.code === "EEXIST") return resolve();
+          return reject(err);
+        }
+
+        // just close the file if it exists
+        fs.close(fd, (err) => {
+          if (err) return reject(err);
+          return resolve();
+        });
+      });
+    });
   }
 }
