69de0670499cf0d2acfbb60be2c0860508c7470a
winterrdog <winterrdog@protonmail.ch>
2024-04-15 12:56:46 +0300

    feat: added logging

--- src/utilities.ts
+++ src/utilities.ts
@@ -4,10 +4,14 @@ import { validationResult } from "express-validator";
 import { JwtPayload } from "./middleware/interfaces";
 import { PathLike } from "fs";
 import * as fs from "fs";
+import { startLogger } from "./logging";
 
+const logger = startLogger(__filename);
 export default class Utility {
   static generateJwtPayload(payload: JwtPayload): Promise<string> {
     return new Promise((resolve, reject) => {
+      logger.info("generating JWT...");
+
       jwt.sign(
         payload,
         process.env.JWT_SECRET as string,
@@ -16,31 +20,55 @@ export default class Utility {
           algorithm: "HS384",
         },
         function (err, token) {
-          return err ? reject(err) : resolve(token as string);
+          if (err) {
+            logger.error("failed to generate jwt token: ", err);
+            reject(err);
+          } else {
+            logger.info("JWT generated successfully!");
+            resolve(token as string);
+          }
+          return;
         }
       );
     });
   }
   static validateRequest(req: Request, res: Response, next: NextFunction) {
+    logger.info("validating and sanitizing request body, query, and params...");
+
     const errors = validationResult(req);
-    return errors.isEmpty()
-      ? next()
-      : res.status(400).json({ errors: errors.array() });
+    if (errors.isEmpty()) {
+      logger.info("request validated successfully!");
+      next();
+    } else {
+      logger.error("request validation failed: ", errors.array());
+      res.status(400).json({ errors: errors.array() });
+    }
   }
   static createFile(fname: PathLike): Promise<void> {
     // create a file if it doesn't exist
     // if it exists, do nothing
     return new Promise((resolve, reject) => {
       // "wx" flag creates a file if it doesn't exist
+      logger.info(`creating log file: ${fname}...`);
       fs.open(fname, "wx", (err, fd) => {
         if (err) {
-          if (err.code === "EEXIST") return resolve();
+          if (err.code === "EEXIST") {
+            logger.info("log file already exists. skipping creation...");
+            return resolve();
+          }
+
+          logger.error("failed to create log file: ", err);
           return reject(err);
         }
 
         // just close the file if it exists
         fs.close(fd, (err) => {
-          if (err) return reject(err);
+          if (err) {
+            logger.error(err, `failed to close file: ${fname}`);
+            return reject(err);
+          }
+
+          logger.info(`log file was already created: ${fname} thus closed it.`);
           return resolve();
         });
       });
