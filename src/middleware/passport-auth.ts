d930b21404c7bf91f1186c9dc877c3eccdfde61e
winterrdog <winterrdog@protonmail.ch>
2024-04-19 14:51:35 +0300

    fix: changed auth failure logic to use a callback

--- src/middleware/passport-auth.ts
+++ src/middleware/passport-auth.ts
@@ -2,28 +2,96 @@ import { NextFunction, Response, Request, Express } from "express";
 import * as passport from "passport";
 import userPassStrategy from "./strategy-user-pass";
 import jwtStrategy from "./strategy-jwt";
+import { startLogger } from "../logging";
 
-const redirectPath = "/api/users/sign-in";
+const logger = startLogger(__filename);
 export function authenticateUserPass(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  return passport.authenticate("local", {
-    failureRedirect: redirectPath,
-    session: false,
-  })(req, res, next);
+  // note: if u used "failureRedirect", that route better be
+  // a GET route otherwise, opt for maximum control thru a callback
+  const authCb: passport.AuthenticateCallback = (
+    err: any,
+    user?: Express.User | false | null,
+    info?: object | string | Array<string | undefined>,
+    status?: number | Array<number | undefined>
+  ): any => {
+    if (err) {
+      info &&
+        logger.error(`error happened in user-pass auth: ${info.toString()}`);
+      return next(err);
+    }
+
+    if (!user) {
+      info &&
+        logger.warn(
+          `something not nice happened in user-pass auth: ${info.toString()}`
+        );
+      if (typeof status === "number") {
+        return res.status(status).json({
+          message: "invalid or corrupted jwt was provided",
+        });
+      }
+      return res.status(401).json({
+        message: "invalid or corrupted jwt was provided",
+      });
+    }
+
+    return next();
+  };
+
+  return passport.authenticate("local", { session: false }, authCb)(
+    req,
+    res,
+    next
+  );
 }
+
 export function authenticateJwt(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  return passport.authenticate("jwt", {
-    failureRedirect: redirectPath,
-    session: false,
-  })(req, res, next);
+  // note: if u used "failureRedirect", that route better be
+  // a GET route otherwise, opt for maximum control thru a callback
+  const authCb: passport.AuthenticateCallback = (
+    err: any,
+    user?: Express.User | false | null,
+    info?: object | string | Array<string | undefined>,
+    status?: number | Array<number | undefined>
+  ): any => {
+    if (err) {
+      info && logger.error(`error happened in JWT auth: ${info.toString()}`);
+      return next(err);
+    }
+
+    if (!user) {
+      info &&
+        logger.warn(
+          `something not nice happened in JWT auth: ${info.toString()}`
+        );
+      if (typeof status === "number") {
+        return res.status(status).json({
+          message: "invalid or corrupted jwt was provided",
+        });
+      }
+      return res.status(401).json({
+        message: "invalid or corrupted jwt was provided",
+      });
+    }
+
+    return next();
+  };
+
+  return passport.authenticate("jwt", { session: false }, authCb)(
+    req,
+    res,
+    next
+  );
 }
+
 export function initialize(app: Express) {
   app.use(passport.initialize());
 
