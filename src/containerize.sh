ec462f889c0ae6433f3f4a4156b9539d661ec38b
winterrdog <winterrdog@protonmail.ch>
2024-06-18 11:01:04 +0300

    fix: added fallback in case docker-compose is not deh

--- src/containerize.sh
+++ src/containerize.sh
@@ -1,4 +1,10 @@
 #/usr/bin/env bash
 
 # start containers via docker compose
-docker-compose up -d
+# if docker-compose is not installed, use "docker compose up -d" instead
+if ! command -v docker-compose &> /dev/null
+then
+    docker compose up -d
+else
+    docker-compose up -d
+fi
