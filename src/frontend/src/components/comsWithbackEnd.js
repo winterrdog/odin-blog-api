e2369bc325fe20f0b4fa578219fa97192401653d
muchubatactics <byooxmckeith@gmail.com>
2024-07-28 16:39:59 +0300

    Add: local storage for log in info

--- src/frontend/src/components/comsWithbackEnd.js
+++ src/frontend/src/components/comsWithbackEnd.js
@@ -1,3 +1,29 @@
 const baseURL = 'http://localhost:3000';
 
-export {baseURL};
+function checkIfLoggedIn() {
+  const accname = localStorage.getItem('accname');
+  console.log(accname);
+  if (accname) return true;
+  else return false;
+}
+
+function setLoggedIn(str) {
+  localStorage.setItem('accname', str);
+}
+
+function getLogInfo() {
+  let obj = {};
+
+  if (checkIfLoggedIn()) {
+    obj.name = localStorage.getItem('accname');
+    console.log(obj);
+  }
+
+  return obj;
+}
+
+function clearMemory() {
+  localStorage.clear();
+}
+
+export {baseURL, setLoggedIn, checkIfLoggedIn, clearMemory, getLogInfo};
