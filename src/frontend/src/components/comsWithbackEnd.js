77a114673853eaff3cc60e17937dc49dc936780e
muchubatactics <byooxmckeith@gmail.com>
2024-08-02 15:39:58 +0300

    Updates: overhaul but minor

--- src/frontend/src/components/comsWithbackEnd.js
+++ src/frontend/src/components/comsWithbackEnd.js
@@ -8,6 +8,7 @@ function checkIfLoggedIn() {
 
 function setLoggedIn(str) {
   localStorage.setItem('accname', str);
+  document.body.style.backgroundColor = 'white';
 }
 
 function setToken(str) {
@@ -31,8 +32,26 @@ function getLogInfo() {
 
 function clearMemory() {
   localStorage.clear();
+  document.body.style.backgroundColor = 'rgb(255, 253, 241)';
 }
+
+let shortCutToSignIn = (() => {
+  let cb = null;
+
+  function setCb(callBack) {
+    cb = callBack;
+  }
+
+  function callCb() {
+    if (cb) cb();
+  }
+
+  return { setCb, callCb};
+})();
 
 // clearMemory();
 
-export {baseURL, setLoggedIn, checkIfLoggedIn, clearMemory, getLogInfo, setToken, getToken};
+if (checkIfLoggedIn()) document.body.style.backgroundColor = 'white';
+else document.body.style.backgroundColor = 'rgb(255, 253, 241)';
+
+export {baseURL, setLoggedIn, checkIfLoggedIn, clearMemory, getLogInfo, setToken, getToken, shortCutToSignIn};
--- src/frontend/src/components/main.jsx
+++ src/frontend/src/components/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider } from "react-router-dom";
+
+import routes from "./routes";
+
 import '../styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </React.StrictMode>,
 )
