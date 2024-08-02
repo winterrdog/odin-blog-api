9f7889391528dc0167bf99eafda2f9eef825ea0b
muchubatactics <byooxmckeith@gmail.com>
2024-08-02 15:38:11 +0300

    Update: modify routes with new components

--- src/frontend/src/components/routes.jsx
+++ src/frontend/src/components/routes.jsx
@@ -1,22 +1,46 @@
 import { createBrowserRouter } from "react-router-dom";
+
 import Homepage from "./homepage";
 import ErrorElement from "./errorElement";
 import Write from "./write";
 import Post from "./post";
+import App from "./App";
+import About from "./about";
+import Posts from "./posts";
+import MyAccount from "./myaccount";
 
 const routes = createBrowserRouter([
     {
       path: '/',
       errorElement: <ErrorElement />,
-      element: <Homepage />,
+      element: <App />,
+      children: [
+        {
+          index: true,
+          path: '',
+          element: <Homepage />
+        },        
+        {
+          path: 'post',
+          element: <Post />
+        },
+        {
+          path: 'posts',
+          element: <Posts />
+        },
+        {
+          path: 'my-account',
+          element: <MyAccount />
+        }
+      ]
     },
     {
       path: '/write',
       element: <Write />
     },
     {
-      path: '/post',
-      element: <Post />
+      path: '/about',
+      element: <About />,
     },
   ], 
   {
