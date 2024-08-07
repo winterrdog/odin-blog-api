bd04751c9307747940541dbb3e13cc664c985ddb
winterrdog <winterrdog@protonmail.ch>
2024-08-07 18:19:57 +0300

    fix: added `TokenExpiredError` handling in `jwt` verification

--- src/middleware/strategy-jwt.ts
+++ src/middleware/strategy-jwt.ts
@@ -1,3 +1,4 @@
+import { TokenExpiredError } from "jsonwebtoken";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { JwtPayload } from "./interfaces";
 import { UserModel } from "../models/user";
@@ -5,6 +6,7 @@ import { startLogger } from "../logging";
 require("dotenv").config();
 
 const logger = startLogger(__filename);
+
 const verifyJwtCb = async (payload: JwtPayload, cb: any) => {
   try {
     logger.info(`verifying jwt for user: ${payload.data.sub}`);
@@ -19,16 +21,24 @@ const verifyJwtCb = async (payload: JwtPayload, cb: any) => {
     (user as any)["data"] = payload;
     return cb(null, user);
   } catch (e) {
-    logger.error(e, "error occurred during jwt verification");
+    if (e instanceof TokenExpiredError) {
+      logger.error(e, "jwt token expired");
+    } else {
+      logger.error(e, "error occurred during jwt verification");
+    }
+
     return cb(e);
   }
 };
+
 const options = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   ignoreExpiration: false,
   secretOrKey: process.env.JWT_SECRET!,
 };
+
 logger.info("setting up jwt strategy...");
+
 const jwtStrategy = new Strategy(options, verifyJwtCb);
 
 export default jwtStrategy;
