1c8a95a2ace4a2215c8a1595c56e8029a4a986be
winterrdog <winterrdog@protonmail.ch>
2025-12-07 21:13:40 +0300

    feat(logger): implement custom logger middleware for request logging

---
+++ src/go-impl/internal/middleware/logger.go
@@ -0,0 +1,23 @@
+package middleware
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupLogger(r *gin.Engine) {
+	var formatter = func(param gin.LogFormatterParams) string {
+		return fmt.Sprintf(`{"time":"%s"|"status":%d|"method":"%s"|"path":"%s"|"latency":"%s"|"ip":"%s"|"user_agent":%q}`+"\n",
+			param.TimeStamp.Format(time.RFC3339),
+			param.StatusCode,
+			param.Method,
+			param.Path,
+			param.Latency,
+			param.ClientIP,
+			param.Request.UserAgent(),
+		)
+	}
+	r.Use(gin.LoggerWithFormatter(formatter))
+}
